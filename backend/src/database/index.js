daaf6216b52e3054e5b833d4f01294dd7d8310e1
Felipe Carvalho <felipecarvalho1915@hotmail.com>
2020-05-02 16:49:51 -0300

    Adds connection and association of database

---
+++ backend/src/database/index.js
@@ -0,0 +1,26 @@
+const Sequelize = require('sequelize')
+const dbConfig = require('../config/database')
+
+const Address = require('../models/Address')
+const Category = require('../models/Category')
+const Product = require('../models/Product')
+const ProductSold = require('../models/ProductSold')
+const Sale = require('../models/Sale')
+const User = require('../models/User')
+
+const connection = new Sequelize(dbConfig)
+
+Address.init(connection)
+Category.init(connection)
+Product.init(connection)
+ProductSold.init(connection)
+Sale.init(connection)
+User.init(connection)
+
+Address.associate(connection.models)
+Category.associate(connection.models)
+Product.associate(connection.models)
+Sale.associate(connection.models)
+User.associate(connection.models)
+
+module.exports = connection
