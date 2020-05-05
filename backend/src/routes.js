64a31b6c953008d9a354d1d2dc7f495ccd10b698
Felipe Carvalho <felipecarvalho1915@hotmail.com>
2020-05-05 16:40:09 -0300

    Adds routes for products

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,6 +3,7 @@ const authMiddleware = require('./middlewares/auth')
 
 const UserController = require('./controllers/UserController')
 const ProfileController = require('./controllers/ProfileController')
+const ProductController = require('./controllers/ProductController')
 
 const routes = express.Router()
 
@@ -10,9 +11,19 @@ routes.get('/users', UserController.index)
 routes.post('/register', UserController.create)
 routes.post('/login', UserController.login)
 
+routes.get('/profile/:id', ProfileController.viewProfile)
+
+routes.get('/products', ProductController.listAllProducts)
+routes.get('/products/category', ProductController.listProductsByCategory)
+routes.get('/products/:productId', ProductController.productInfo)
+
 routes.use(authMiddleware)
 routes.get('/profile', ProfileController.index)
+routes.post('/profile/info', ProfileController.createProfile)
 routes.put('/profile/edit', ProfileController.updateProfile)
-routes.post('/register/info', ProfileController.create)
+
+routes.post('/profile/product', ProductController.createProduct)
+routes.get('/profile/product', ProductController.listUserProducts)
+
 
 module.exports = routes
