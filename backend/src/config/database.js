d38fe5082d92ea6c207331764f995cef71056dbb
Felipe Carvalho <felipecarvalho1915@hotmail.com>
2020-05-06 17:19:36 -0300

    Updated database file

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
     dialect: 'postgres',
-    host: 'localhost',
-    username: 'postgres',
-    password: 123456,
-    database: 'ecommerce',
+    host: process.env.DATABASE_HOST,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
     define: {
         timestamps: true,
         underscored: true
