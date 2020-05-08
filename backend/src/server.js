78832fb967b86f00f27d5f5e3d006ff2d15f9557
Felipe Carvalho <felipecarvalho1915@hotmail.com>
2020-05-08 15:24:29 -0300

    Adds cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,13 @@
 require('dotenv').config()
+require('./database')
 
 const express = require('express')
 const routes = require('./routes')
-
-require('./database')
+const cors = require('cors')
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
