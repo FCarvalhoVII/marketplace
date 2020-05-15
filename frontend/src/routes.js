d2d10f3cbf8a4ad67daa6bce16459fd154da2064
Felipe Carvalho <felipecarvalho1915@hotmail.com>
2020-05-15 17:21:51 -0300

    Adds products by category and profile routes

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,8 +2,10 @@ import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import Home from './pages/Home'
+import ProductsByCategory from './pages/ByCategory'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import Profile from './pages/Profile'
 import CreateAddress from './pages/Profile/Create'
 
 function Routes() {
@@ -11,8 +13,10 @@ function Routes() {
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact component={Home} />
+                <Route path="/category/:categoryId" children={<ProductsByCategory />} />
                 <Route path="/login" component={Login} />
                 <Route path="/register" component={Register} />
+                <Route path="/profile" component={Profile} />
                 <Route path="/address" component={CreateAddress} />
             </Switch>
         </BrowserRouter>
