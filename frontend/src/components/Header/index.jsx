64cacc342aabcbd6c166ad49601a26f8adb27611
Felipe Carvalho <felipecarvalho1915@hotmail.com>
2020-05-19 16:39:24 -0300

    Adds Logout function

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 import { FcElectronics } from 'react-icons/fc'
 import { MdShoppingCart } from 'react-icons/md'
+
+import LoginButtons from './LoginButtons'
 
 import './styles.css'
 
 function Header() {
+    const userId = localStorage.getItem('token')
+    const email = localStorage.getItem('user')
+
+    const history = useHistory()
+
+    function handleLogout() {
+        localStorage.clear()
+
+        history.push('/')
+    }
+    
     return (
         <header>
             <div className="logo">
@@ -14,12 +27,21 @@ function Header() {
                 </Link>
 
                 <div className="buttons-container">
-                    <Link to="/register">
-                        <button className="button-register">Registrar</button>
-                    </Link>
-                    <Link to="/login">
-                        <button className="button-login">Entrar</button>
-                    </Link>
+
+                    {userId  
+                        ? <div className="toggle-profile">
+                            <div className="user-mini-img">
+                                <img src={null} alt=""/>
+                            </div>
+                            <Link to="/profile">
+                                <h3>Seu Perfil</h3>
+                                <p>{ email }</p>
+                            </Link>
+                            <button className="button-logout" onClick={handleLogout}>Sair</button>
+                        </div>
+                        : <LoginButtons />
+                    }
+
                 </div>
             </div>
 
