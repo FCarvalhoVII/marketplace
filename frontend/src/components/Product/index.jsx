a184422b7505458ee922a7df6746236408785cc2
Felipe Carvalho <felipecarvalho1915@hotmail.com>
2020-05-21 16:35:48 -0300

    Adds Link to Product page

--- frontend/src/components/Product/index.jsx
+++ frontend/src/components/Product/index.jsx
@@ -1,30 +1,33 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import './styles.css'
 
-function Product({ name, price, salesman }) {
+function Product({ name, price, salesman, productId }) {
     return (
-        <div className="product">
-            <div className="product-img">
-                <img src={null} alt=""/>
-            </div>
+        <Link to={`/product/${productId}`}>
+            <div className="product">
+                <div className="product-img">
+                    <img src={null} alt=""/>
+                </div>
 
-            <div className="product-info">
-                <h3>{ name }</h3>
+                <div className="product-info">
+                    <h3>{ name }</h3>
 
-                <p>
-                    {
-                        Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' })
-                            .format(price)
-                    }
-                </p>
-                
-                <div className="salesman-info">
-                    <p>Vendedor:</p>
-                    <p>{ salesman }</p>
+                    <p>
+                        {
+                            Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' })
+                                .format(price)
+                        }
+                    </p>
+                    
+                    <div className="salesman-info">
+                        <p>Vendedor:</p>
+                        <p>{ salesman }</p>
+                    </div>
                 </div>
             </div>
-        </div>
+        </Link>
     )
 }
 
