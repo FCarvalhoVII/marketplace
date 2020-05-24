989040d82e618a91e7f3acd5b464740e81d6fdd1
Felipe Carvalho <felipecarvalho1915@hotmail.com>
2020-05-24 16:23:22 -0300

    Adds ProductsSold component

---
+++ frontend/src/components/User/ProductsSoldContent/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import './styles.css'
+
+function UserContent({ name, city, products }) {
+    return (
+        <div className="profile-container">
+            <h2>{ name }</h2>
+
+            <h3>{ city }</h3>
+
+            <ul>
+                <h3>Produtos vendidos:</h3>
+
+                {products.map(product => (                
+                    <li  key={product.id}>
+                        <div className="mini-img">
+                            <img src={null} alt=""/>
+                        </div>
+
+                        <span>{product.products.name}</span>
+
+                        <span>Quantidade comprada: {product.quantity_sold}</span>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default UserContent
